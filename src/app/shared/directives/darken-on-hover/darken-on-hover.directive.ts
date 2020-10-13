a482055a570f470d2a44633f72b9c577e6fa18f5
Bruno Santana <brunoalexandre313@gmail.com>
2020-10-12 23:04:10 -0300

    Cria alurapic

---
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appDarkenOnHover]'
+})
+export class DarkenOnHoverDirective {
+
+  @Input() brightness = '70%';
+
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) { }
+
+  @HostListener('mouseover')
+  darkenOn() {
+    this.renderer.setStyle(this.el.nativeElement, 'filter', `brightness(${this.brightness})`);
+  }
+
+  @HostListener('mouseleave')
+  darkenOff() {
+    this.renderer.setStyle(this.el.nativeElement, 'filter', 'brightness(100%)');
+  }
+
+}
